52918ffe0269f36e31b4cc77f2e437a982f3be76
dujiande <jiande@zkjinshi.com>
2015-11-16 15:33:42 +0800

    update the net

--- SVIPApp/src/main/java/com/zkjinshi/svip/net/ExtNetRequestListener.java
+++ SVIPApp/src/main/java/com/zkjinshi/svip/net/ExtNetRequestListener.java
@@ -41,16 +41,21 @@ public abstract class ExtNetRequestListener  implements NetRequestListener{
 
     @Override
     public void onNetworkResponseSucceed(NetResponse result) {
-        BaseBean baseBean = new Gson().fromJson(result.rawResult,BaseBean.class);
-        if(baseBean!= null && !baseBean.isSet() && baseBean.getErr().equals("400")){
-            if(context instanceof Activity){
-                DialogUtil.getInstance().showToast(context, "Token失效，请重新登录!");
-                CacheUtil.getInstance().setLogin(false);
-                Intent intent = new Intent(context,LoginActivity.class);
-                context.startActivity(intent);
-                ((Activity)context).finish();
+        try{
+            BaseBean baseBean = new Gson().fromJson(result.rawResult,BaseBean.class);
+            if(baseBean!= null && !baseBean.isSet() && baseBean.getErr().equals("400")){
+                if(context instanceof Activity){
+                    DialogUtil.getInstance().showToast(context, "Token失效，请重新登录!");
+                    CacheUtil.getInstance().setLogin(false);
+                    Intent intent = new Intent(context,LoginActivity.class);
+                    context.startActivity(intent);
+                    ((Activity)context).finish();
+                }
             }
+        }catch (Exception e){
+            Log.e(ExtNetRequestListener.class.getSimpleName(),e.getMessage());
         }
+
     }
 
     @Override
