ee1afc6255e2eeecbf1a425ee573d0df6d90ea59
JimmyZhang <jimmyzhang144@gmail.com>
2015-12-03 14:19:30 +0800

    增加图片缓存操作

--- SVIPApp/src/main/java/com/zkjinshi/svip/adapter/ChatAdapter.java
+++ SVIPApp/src/main/java/com/zkjinshi/svip/adapter/ChatAdapter.java
@@ -444,7 +444,7 @@ public class ChatAdapter extends BaseAdapter {
             }
             vh.msg.setText("");
             vh.msg.setVisibility(View.GONE);
-            String key = message.getMsgId();
+            final String key = message.getMsgId();
             Bitmap displayBitmap = null;
             Bitmap bitmapCache = (Bitmap) msgCacheMap.get(key);
             final String fileName = imgBody.getFileName();
@@ -453,17 +453,13 @@ public class ChatAdapter extends BaseAdapter {
                 if(!TextUtils.isEmpty(filePath)){
                     displayBitmap = BitmapFactory.decodeFile(filePath);
                 }
-                if(null == displayBitmap){
-                    displayBitmap = BitmapFactory.decodeFile(FileUtil.getInstance()
-                            .getImagePath() + fileName);
-                }
                 if (displayBitmap != null) {// 本地取
                     displayBitmap = ImageUtil.cropThumbBitmap(displayBitmap);
                     displayBitmap = ImageUtil.loadThumbBitmap(context,
                             displayBitmap);
+                    msgCacheMap.put(key,displayBitmap);
                     vh.img.setImageBitmap(displayBitmap);
                 } else {// 从网络取
-                    //ToDo Jimmy 获取图片链接
                     String thumbUrl =  imgBody.getThumbnailUrl();
                     if(!TextUtils.isEmpty(thumbUrl)){
                         ImageLoader.getInstance().displayImage(thumbUrl, vh.img,
@@ -487,6 +483,7 @@ public class ChatAdapter extends BaseAdapter {
                                                         + fileName);
                                         ImageUtil.saveBitmap(loadedImage,
                                                 file.getPath());
+                                        msgCacheMap.put(key,loadedImage);
                                     }
 
                                     @Override
