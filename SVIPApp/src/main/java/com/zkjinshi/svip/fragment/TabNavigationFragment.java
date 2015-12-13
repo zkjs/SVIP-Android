c8531ff5a589c9d06e149fa6e6dd297c7bbb24c6
JimmyZhang <jimmyzhang144@gmail.com>
2015-12-13 15:15:27 +0800

    完成底部栏框架调整

--- SVIPApp/src/main/java/com/zkjinshi/svip/fragment/TabNavigationFragment.java
+++ SVIPApp/src/main/java/com/zkjinshi/svip/fragment/TabNavigationFragment.java
@@ -46,34 +46,8 @@ import java.util.ArrayList;
 public class TabNavigationFragment extends Fragment implements
 		RadioGroup.OnCheckedChangeListener {
 
-
 	public static final String TAG = TabNavigationFragment.class.getSimpleName();
-	public SVIPApplication svipApplication;
-	public OrderLastResponse lastOrderInfo = null;
-	public ListenerDialog listenerDialog;
-
-	private static final int LONG_CLICK_TIME = 500;
-	private static final int LONG_CLICK_MSG = 1;
-	private static final int SHORT_CLICK_MSG = 2;
-	private static final int LONG_CLICK_UP_MSG = 3;
-	public boolean isLongClick = false;
-	Handler handler = new Handler(){
-		public void handleMessage(Message msg){
-			if(msg.what == LONG_CLICK_MSG){
-				isLongClick = true;
-				longClickDeal();
-			}else if(msg.what == SHORT_CLICK_MSG){
-				isLongClick = false;
-				shortClickDeal();
-			}else if(msg.what == LONG_CLICK_UP_MSG){
-				isLongClick = false;
-				longClickUpDeal();
-			}
-		}
-	};
-
 	private RadioGroup mTabItemGroup;
-	private ImageView logoIv;
 
 	private SparseArray<Fragment> mContentFragments = new SparseArray<Fragment>(
 			4);
@@ -95,118 +69,6 @@ public class TabNavigationFragment extends Fragment implements
 		super.onStart();
 	}
 
-	public void longClickDeal(){
-//		if(!listenerDialog.isShowing()){
-//			listenerDialog.show();
-//			listenerDialog.startRecord();
-//		}
-	}
-
-	public void longClickUpDeal(){
-//		if(listenerDialog.isShowing()){
-//			listenerDialog.stopRecord();
-//			listenerDialog.cancel();
-//		}
-//		final String shopid = getCleverShopId();
-//		loadCleverServer(shopid);
-	}
-
-	public void shortClickDeal(){
-		new CleverDialog(getActivity()).show();
-	}
-
-	@Override
-	public void onPause() {
-		super.onPause();
-		if(listenerDialog.isShowing()){
-			longClickUpDeal();
-		}
-	}
-
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		if(listenerDialog != null){
-			listenerDialog.stopAnimation();
-		}
-	}
-
-	//智能选择酒店
-	public String getCleverShopId(){
-		if(getActivity() instanceof MainActivity){
-			MainActivity mainActivity = (MainActivity)getActivity();
-			lastOrderInfo = mainActivity.lastOrderInfo;
-		}
-		if(lastOrderInfo != null){
-			return lastOrderInfo.getShopid();
-		}else if(svipApplication.mRegionList.size() > 0){
-			RegionVo region = svipApplication.mRegionList.get(svipApplication.mRegionList.size() - 1);
-			return  region.getiBeacon().getShopid();
-		}
-		return "120";
-	}
-
-	//智能选择客服
-	public void loadCleverServer(final String shopid){
-		CustomerServicesManager.getInstance().requestServiceListTask(getActivity(),shopid , new ExtNetRequestListener(getActivity()) {
-			@Override
-			public void onNetworkRequestError(int errorCode, String errorMessage) {
-				Log.i(TAG, "errorCode:" + errorCode);
-				Log.i(TAG, "errorMessage:" + errorMessage);
-			}
-
-			@Override
-			public void onNetworkRequestCancelled() {
-
-			}
-
-			@Override
-			public void onNetworkResponseSucceed(NetResponse result) {
-				Log.i(TAG, "result:" + result.rawResult);
-				CustomerServiceListResponse customerServiceListResponse = new Gson().fromJson(result.rawResult, CustomerServiceListResponse.class);
-				if (null != customerServiceListResponse) {
-					HeadBean head = customerServiceListResponse.getHead();
-					if (null != head) {
-						boolean isSet = head.isSet();
-						if (isSet) {
-							ArrayList<CustomerServiceBean> customerServiceList = customerServiceListResponse.getData();
-							String salesId = head.getExclusive_salesid();
-							CustomerServiceBean customerService = null;
-							if (null != customerServiceList && !customerServiceList.isEmpty()) {
-								if (!TextUtils.isEmpty(salesId)) {//有专属客服
-									customerService = CustomerServicesManager.getInstance().getExclusiveCustomerServic(customerServiceList, salesId);
-								} else {//无专属客服
-									customerService = CustomerServicesManager.getInstance().getRandomCustomerServic(customerServiceList);
-								}
-								showBookingDailog(shopid,customerService);
-							}
-						}
-					}
-				}
-			}
-
-			@Override
-			public void beforeNetworkRequestStart() {
-
-			}
-		});
-	}
-
-	public void showBookingDailog(String shopId,CustomerServiceBean customerServiceBean){
-		BookingDialog bookingDialog = new BookingDialog(getActivity());
-		bookingDialog.shopId = shopId;
-		bookingDialog.customerService = customerServiceBean;
-		bookingDialog.shopName = ShopDetailDBUtil.getInstance().queryShopNameByShopID(shopId);
-		if(listenerDialog != null && !TextUtils.isEmpty(listenerDialog.getMediaPath())){
-			bookingDialog.recordFileName = listenerDialog.getMediaPath();
-			bookingDialog.recordSecond = listenerDialog.getRecordSecond();
-			listenerDialog.setRecordFileName(null);
-		}
-		bookingDialog.show();
-	}
-
-
-
 	public static void setCurrentNavigationChecked(int rbId, Activity activity) {
 		RadioGroup mTabItemGroup = (RadioGroup) activity
 				.findViewById(R.id.footer_tab_rg);
@@ -220,31 +82,6 @@ public class TabNavigationFragment extends Fragment implements
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.footer_tab, container, false);
-		svipApplication = (SVIPApplication)getActivity().getApplication();
-		listenerDialog = new ListenerDialog(getActivity());
-		logoIv = (ImageView)view.findViewById(R.id.footer_tab_iv_zhijian);
-		logoIv.setOnTouchListener(new View.OnTouchListener() {
-			@Override
-			public boolean onTouch(View view, MotionEvent event) {
-				switch (event.getAction()) {
-					case MotionEvent.ACTION_DOWN:
-						handler.sendEmptyMessageDelayed(LONG_CLICK_MSG, LONG_CLICK_TIME);
-						return true;
-					case MotionEvent.ACTION_UP:
-						handler.removeMessages(LONG_CLICK_MSG);
-						if(isLongClick){
-							isLongClick = false;
-							handler.sendEmptyMessage(LONG_CLICK_UP_MSG);
-							return false;
-						}
-						isLongClick = false;
-						handler.sendEmptyMessage(SHORT_CLICK_MSG);
-						return true;
-
-				}
-				return false;
-			}
-		});
 		return view;
 	}
 
