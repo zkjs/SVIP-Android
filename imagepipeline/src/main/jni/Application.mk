62ceed0cf31d9c18dc5d681fcbc70d7aba98d75c
JimmyZhang <jimmyzhang144@gmail.com>
2016-03-02 12:52:23 +0800

    修改配置项

--- imagepipeline/src/main/jni/Application.mk
+++ imagepipeline/src/main/jni/Application.mk
@@ -1,7 +1,7 @@
 # Copyright 2004-present Facebook. All Rights Reserved.
 APP_BUILD_SCRIPT := Android.mk
 
-APP_ABI := armeabi x86
+APP_ABI := all
 APP_PLATFORM := android-9
 
 APP_MK_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
