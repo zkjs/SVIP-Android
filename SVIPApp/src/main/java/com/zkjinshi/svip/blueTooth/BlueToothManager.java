c8692aed6532c53f9ee9ffc365a99f3a5ca7808c
JimmyZhang <jimmyzhang144@gmail.com>
2016-03-11 17:28:08 +0800

    完成蓝牙推送相关管理相关逻辑

--- SVIPApp/src/main/java/com/zkjinshi/svip/blueTooth/BlueToothManager.java
+++ SVIPApp/src/main/java/com/zkjinshi/svip/blueTooth/BlueToothManager.java
@@ -1,28 +1,16 @@
 package com.zkjinshi.svip.blueTooth;
 
 import android.content.Context;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.text.TextUtils;
 import android.util.Log;
 
-import com.amap.api.location.AMapLocation;
-import com.amap.api.location.AMapLocationClient;
-import com.amap.api.location.AMapLocationClientOption;
-import com.amap.api.location.AMapLocationListener;
-import com.google.gson.Gson;
 import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.JsonHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
-import com.loopj.android.http.ResponseHandlerInterface;
 import com.zkjinshi.base.log.LogLevel;
 import com.zkjinshi.base.log.LogUtil;
-import com.zkjinshi.base.util.DeviceUtils;
 import com.zkjinshi.pyxis.bluetooth.IBeaconContext;
 import com.zkjinshi.pyxis.bluetooth.IBeaconController;
 import com.zkjinshi.pyxis.bluetooth.IBeaconObserver;
@@ -30,31 +18,24 @@ import com.zkjinshi.pyxis.bluetooth.IBeaconService;
 import com.zkjinshi.pyxis.bluetooth.IBeaconSubject;
 import com.zkjinshi.pyxis.bluetooth.IBeaconVo;
 import com.zkjinshi.pyxis.bluetooth.NetBeaconVo;
-import com.zkjinshi.svip.SVIPApplication;
-import com.zkjinshi.svip.activity.common.MainController;
-import com.zkjinshi.svip.bean.LocPushBean;
+import com.zkjinshi.svip.manager.BleLogManager;
+import com.zkjinshi.svip.manager.BleStatManager;
 import com.zkjinshi.svip.net.ExtNetRequestListener;
 import com.zkjinshi.svip.net.MethodType;
 import com.zkjinshi.svip.net.NetRequest;
 import com.zkjinshi.svip.net.NetRequestTask;
 import com.zkjinshi.svip.net.NetResponse;
-import com.zkjinshi.svip.net.SvipHttpClient;
+import com.zkjinshi.svip.sqlite.BleStatDBUtil;
 import com.zkjinshi.svip.utils.CacheUtil;
 import com.zkjinshi.svip.utils.ProtocolUtil;
 
-import org.eclipse.paho.client.mqttv3.IMqttActionListener;
-import org.eclipse.paho.client.mqttv3.IMqttToken;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import cz.msebera.android.httpclient.Header;
 import cz.msebera.android.httpclient.entity.StringEntity;
-import io.yunba.android.manager.YunBaManager;
 
 /**
  * 蓝牙管理器
@@ -90,8 +71,6 @@ public class BlueToothManager {
         @Override
         public void intoRegion(IBeaconVo iBeaconVo) {
             LogUtil.getInstance().info(LogLevel.DEBUG,"进入："+iBeaconVo);
-            //pushIbeacon(iBeaconVo);
-            //lbsLocBeaconRequest(iBeaconVo);
             Bundle bundle = new Bundle();
             bundle.putSerializable("iBeaconVo",iBeaconVo);
             Message msg = new Message();
@@ -105,66 +84,6 @@ public class BlueToothManager {
             LogUtil.getInstance().info(LogLevel.DEBUG,"离开："+iBeaconVo);
         }
     };
-
-
-    public void pushIbeacon(IBeaconVo iBeaconVo){
-        try {
-            String url = ProtocolUtil.lbsLocBeacon();
-            NetRequest netRequest = new NetRequest(url);
-            HashMap<String,Object> bizMap = new HashMap<String,Object>();
-            bizMap.put("locid",iBeaconVo.getMajor()+"");
-            bizMap.put("major", iBeaconVo.getMajor()+"");
-            bizMap.put("minor", iBeaconVo.getMinor()+"");
-            bizMap.put("uuid", iBeaconVo.getProximityUuid());
-            bizMap.put("sensorid", iBeaconVo.getBluetoothAddress());
-            bizMap.put("token", CacheUtil.getInstance().getExtToken());
-            bizMap.put("timestamp", iBeaconVo.getTimestamp());
-
-            /*bizMap.put("locid","1");
-            bizMap.put("major","2");
-            bizMap.put("minior", "3");
-            bizMap.put("uuid", "uuid-uuid-uuid-uuid");
-            bizMap.put("sensorid","sensorid");
-            bizMap.put("token", "head.payload.sign");
-            bizMap.put("timestamp", 1455870706863L);*/
-
-            netRequest.setObjectParamMap(bizMap);
-            NetRequestTask netRequestTask = new NetRequestTask(context,netRequest, NetResponse.class);
-            netRequestTask.methodType = MethodType.PUT;
-            LogUtil.getInstance().info(LogLevel.DEBUG,"调用API："+url);
-            LogUtil.getInstance().info(LogLevel.DEBUG,"API推送参数:"+bizMap.toString());
-            netRequestTask.setNetRequestListener(new ExtNetRequestListener(context) {
-                @Override
-                public void onNetworkRequestError(int errorCode, String errorMessage) {
-                    Log.i(TAG, "errorCode:" + errorCode);
-                    Log.i(TAG, "errorMessage:" + errorMessage);
-                }
-
-                @Override
-                public void onNetworkRequestCancelled() {
-
-                }
-
-                @Override
-                public void onNetworkResponseSucceed(NetResponse result) {
-                    super.onNetworkResponseSucceed(result);
-                    //Log.i(TAG, "result.rawResult:" + result.rawResult);
-                    LogUtil.getInstance().info(LogLevel.DEBUG,"API推送成功");
-                }
-
-                @Override
-                public void beforeNetworkRequestStart() {
-
-                }
-            });
-            netRequestTask.isShowLoadingDialog = false;
-            netRequestTask.execute();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
 
     public void lbsLocBeaconRequest(IBeaconVo iBeaconVo){
         try {
@@ -188,23 +107,22 @@ public class BlueToothManager {
                 public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                     Log.d(TAG,response.toString());
                     LogUtil.getInstance().info(LogLevel.DEBUG,"蓝牙推送成功");
+                    BleStatDBUtil.getInstance().updateTotalCount();
                 }
 
                 @Override
                 public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse){
-                    Log.d(TAG,errorResponse.toString());
                     LogUtil.getInstance().info(LogLevel.DEBUG,"蓝牙推送失败");
+                    BleLogManager.getInstance().collectBleLog(context,throwable.getMessage());
                 }
 
-                public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable){
-                    Log.d(TAG,responseString);
-                    LogUtil.getInstance().info(LogLevel.DEBUG,"蓝牙推送失败");
-                }
 
                 @Override
                 public void onRetry(int retryNo) {
                     Log.d(TAG,"retryNo:"+retryNo);
                     LogUtil.getInstance().info(LogLevel.DEBUG,"蓝牙推送重连"+ retryNo );
+                    BleStatManager.getInstance().updateRetryCount();
+                    BleStatDBUtil.getInstance().updateTotalCount();
 
                 }
             });
@@ -212,16 +130,6 @@ public class BlueToothManager {
             e.printStackTrace();
         }
 
-    }
-
-    public String getResponseString(byte[] responseBody){
-        String result = "";
-        try {
-            result = new String(responseBody,"utf-8");
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return  result;
     }
 
     public static synchronized BlueToothManager getInstance(){
