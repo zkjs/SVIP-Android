57dcf5087de142707423bb353cf961e44b107f83
dujiande <jiande@zkjinshi.com>
2016-03-31 13:15:45 +0800

    登录API修改

--- SVIPApp/src/main/java/com/zkjinshi/svip/activity/common/LoginController.java
+++ SVIPApp/src/main/java/com/zkjinshi/svip/activity/common/LoginController.java
@@ -126,62 +126,6 @@ public class LoginController {
                 }
             });
 
-
-
-
-
-
-            client.get(mContext,url, stringEntity, "application/json", new JsonHttpResponseHandler(){
-                public void onStart(){
-                    super.onStart();
-                    DialogUtil.getInstance().showAvatarProgressDialog(mContext,"");
-                }
-
-                public void onFinish(){
-                    super.onFinish();
-                    DialogUtil.getInstance().cancelProgressDialog();
-                }
-
-                public void onSuccess(int statusCode, Header[] headers, JSONObject response){
-                    super.onSuccess(statusCode,headers,response);
-                    try {
-                        if(response.getInt("res") == 0){
-                            JSONArray dataArr = response.getJSONArray("data");
-                            if(dataArr != null && dataArr.length() > 0){
-                                JSONObject dataJson = dataArr.getJSONObject(0);
-                                CacheUtil.getInstance().setUserPhone(dataJson.getString("phone"));
-                                CacheUtil.getInstance().setUserName(dataJson.getString("username"));
-                                String imgurl = dataJson.getString("userimage");
-                                imgurl = ProtocolUtil.getHostImgUrl(imgurl);
-                                CacheUtil.getInstance().saveUserPhotoUrl(imgurl);
-                                CacheUtil.getInstance().setSex(dataJson.getString("sex"));
-                               // CacheUtil.getInstance().setUserRealName(dataJson.getString("realname"));
-                                CacheUtil.getInstance().setUserApplevel(dataJson.getString("viplevel"));
-                                //订阅云巴区域
-                                YunBaSubscribeManager.getInstance().setAlias(mContext,CacheUtil.getInstance().getUserId());
-
-                                String userid = CacheUtil.getInstance().getUserId();
-                                DBOpenHelper.DB_NAME = userid +".db";
-                                CacheUtil.getInstance().setLogin(true);
-                                if(callBackListener != null){
-                                    callBackListener.successCallback(dataJson);
-                                }
-                            }
-
-                        }else{
-                            Toast.makeText(mContext,response.getString("resDesc"),Toast.LENGTH_SHORT).show();
-                        }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse){
-                    super.onFailure(statusCode,headers,throwable,errorResponse);
-                    Toast.makeText(mContext,"API 错误："+statusCode,Toast.LENGTH_SHORT).show();
-                    AsyncHttpClientUtil.onFailure((Activity) mContext,statusCode);
-                }
-            });
         }catch (Exception e){
             e.printStackTrace();
         }
