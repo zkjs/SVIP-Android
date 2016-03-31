cb3efccfe62516a44da97e3d2751baae08cdb493
dujiande <jiande@zkjinshi.com>
2016-03-31 13:21:56 +0800

    登录API修改

---
+++ SVIPApp/src/main/java/com/zkjinshi/svip/vo/UserInfoVo.java
@@ -0,0 +1,64 @@
+package com.zkjinshi.svip.vo;
+
+import java.io.Serializable;
+
+/**
+ * Created by dujiande on 2016/3/31.
+ */
+public class UserInfoVo implements Serializable {
+
+    private String phone;
+    private String username;
+    private String userimage;
+    private int sex;
+    private String realname;
+    private String viplevel;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserimage() {
+        return userimage;
+    }
+
+    public void setUserimage(String userimage) {
+        this.userimage = userimage;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    public void setRealname(String realname) {
+        this.realname = realname;
+    }
+
+    public String getViplevel() {
+        return viplevel;
+    }
+
+    public void setViplevel(String viplevel) {
+        this.viplevel = viplevel;
+    }
+}
