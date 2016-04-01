05cd334a8aa836efe2280d4b6e1b46c4a9f901fd
dujiande <jiande@zkjinshi.com>
2016-04-01 14:38:40 +0800

    去掉dist参数

--- SVIPApp/src/main/java/com/zkjinshi/svip/utils/ProtocolUtil.java
+++ SVIPApp/src/main/java/com/zkjinshi/svip/utils/ProtocolUtil.java
@@ -42,7 +42,7 @@ public class ProtocolUtil {
      * @return
      */
     public static String ssoVcodeLogin(){
-        return ConfigUtil.getInst().getPavDomain()+"sso/vcode/v1/si?source=login&dist=568e8db288b8a95d7ecfeb9a5d6936b9c521253f3cad30cd9b83ed2d87db9605";
+        return ConfigUtil.getInst().getPavDomain()+"sso/vcode/v1/si?source=login";
     }
 
     /**
@@ -50,7 +50,7 @@ public class ProtocolUtil {
      * @return
      */
     public static String ssoVcodeRegister(){
-        return ConfigUtil.getInst().getPavDomain()+"sso/vcode/v1/si?source=register&dist=568e8db288b8a95d7ecfeb9a5d6936b9c521253f3cad30cd9b83ed2d87db9605";
+        return ConfigUtil.getInst().getPavDomain()+"sso/vcode/v1/si?source=register";
     }
 
     /**
@@ -58,7 +58,7 @@ public class ProtocolUtil {
      * @return
      */
     public static String ssoToken(){
-        return ConfigUtil.getInst().getPavDomain()+"sso/token/v1/phone/si?dist=568e8db288b8a95d7ecfeb9a5d6936b9c521253f3cad30cd9b83ed2d87db9605";
+        return ConfigUtil.getInst().getPavDomain()+"sso/token/v1/phone/si";
     }
 
     /**
@@ -74,7 +74,7 @@ public class ProtocolUtil {
      * @return
      */
     public static String registerSi(){
-        return ConfigUtil.getInst().getPavDomain()+"res/v1/register/si?dist=568e8db288b8a95d7ecfeb9a5d6936b9c521253f3cad30cd9b83ed2d87db9605";
+        return ConfigUtil.getInst().getPavDomain()+"res/v1/register/si";
     }
 
     /**
