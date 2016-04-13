e6e260490ebfddb3e99f24f6849504f38cb2f41a
JimmyZhang <jimmyzhang144@gmail.com>
2016-04-13 11:04:38 +0800

    新增友盟统计和多渠道打包

--- SVIPApp/src/main/java/com/zkjinshi/svip/base/BaseActivity.java
+++ SVIPApp/src/main/java/com/zkjinshi/svip/base/BaseActivity.java
@@ -3,6 +3,7 @@ package com.zkjinshi.svip.base;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.umeng.analytics.MobclickAgent;
 
 
 /**
@@ -29,12 +30,12 @@ public class BaseActivity extends Activity {
 
     protected void onResume(){
         super.onResume();
-
+        MobclickAgent.onResume(this);
     }
 
     protected void onPause(){
         super.onPause();
-
+        MobclickAgent.onPause(this);
     }
 
     /**
--- SVIPApp/src/main/java/com/zkjinshi/svip/base/BaseFragmentActivity.java
+++ SVIPApp/src/main/java/com/zkjinshi/svip/base/BaseFragmentActivity.java
@@ -3,6 +3,7 @@ package com.zkjinshi.svip.base;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.umeng.analytics.MobclickAgent;
 
 
 /**
@@ -29,12 +30,12 @@ public class BaseFragmentActivity extends FragmentActivity {
 
     protected void onResume(){
         super.onResume();
-
+        MobclickAgent.onResume(this);
     }
 
     protected void onPause(){
         super.onPause();
-
+        MobclickAgent.onPause(this);
     }
 
     /**
