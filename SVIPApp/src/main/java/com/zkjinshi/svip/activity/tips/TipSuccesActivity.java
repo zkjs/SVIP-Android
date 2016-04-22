6c8b4c7c24a118b763b8201cb94e0bad9a430bb4
JimmyZhang <jimmyzhang144@gmail.com>
2016-04-22 11:23:27 +0800

    新增服务员列表页面和小费支付成功页面

---
+++ SVIPApp/src/main/java/com/zkjinshi/svip/activity/tips/TipSuccesActivity.java
@@ -0,0 +1,20 @@
+package com.zkjinshi.svip.activity.tips;
+
+import android.os.Bundle;
+
+import com.zkjinshi.svip.base.BaseActivity;
+
+/**
+ * 小费支付成功页面
+ * 开发者：JimmyZhang
+ * 日期：2016/4/22
+ * Copyright (C) 2016 深圳中科金石科技有限公司
+ * 版权所有
+ */
+public class TipSuccesActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ SVIPApp/src/main/java/com/zkjinshi/svip/activity/tips/WaiterListActivity.java
@@ -0,0 +1,20 @@
+package com.zkjinshi.svip.activity.tips;
+
+import android.os.Bundle;
+
+import com.zkjinshi.svip.base.BaseActivity;
+
+/**
+ * 服务员列表页面
+ * 开发者：JimmyZhang
+ * 日期：2016/4/22
+ * Copyright (C) 2016 深圳中科金石科技有限公司
+ * 版权所有
+ */
+public class WaiterListActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
