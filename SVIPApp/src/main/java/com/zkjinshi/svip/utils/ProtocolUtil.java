56ac9f22c3b75fce7f34f4bcbf34a617cd0c6e63
dujiande <jiande@zkjinshi.com>
2016-06-13 11:27:51 +0800

    完成自动更新功能

--- SVIPApp/src/main/java/com/zkjinshi/svip/utils/ProtocolUtil.java
+++ SVIPApp/src/main/java/com/zkjinshi/svip/utils/ProtocolUtil.java
@@ -209,7 +209,7 @@ public class ProtocolUtil {
     /**
      * 获取升级版本信息
      * @param apptype 1 (int required) - 应用类型： 1 超级身份 2 超级服务
-     * @param devicetype IOS (String required) - 设备类型：IOS ANDROID
+     * @param devicetype IOS (String required) - 设备类型：IOS ANDROID AND_PAD
      * @param verno 1.0.0.1 (String required) - 当前客户端版本号
      * @return
      */
