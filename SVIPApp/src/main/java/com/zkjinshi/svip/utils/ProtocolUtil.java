242abc9538c2221f60ec62bb632ca06249b2792a
dujiande <jiande@zkjinshi.com>
2016-06-17 11:02:27 +0800

    bug fix

--- SVIPApp/src/main/java/com/zkjinshi/svip/utils/ProtocolUtil.java
+++ SVIPApp/src/main/java/com/zkjinshi/svip/utils/ProtocolUtil.java
@@ -208,7 +208,7 @@ public class ProtocolUtil {
 
     /**
      * 获取升级版本信息
-     * @param apptype 1 (int required) - 应用类型： 1 超级身份 2 超级服务
+     * @param apptype 1 (int required) - 应用类型： 1 超级身份 2 超级服务 3.静享 4.超级锁屏 5.手势游戏
      * @param devicetype IOS (String required) - 设备类型：IOS ANDROID AND_PAD
      * @param verno 1.0.0.1 (String required) - 当前客户端版本号
      * @return
